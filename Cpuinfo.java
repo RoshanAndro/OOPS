966196658eeeb1600e75ec45d4544b49a44fdea7
Mohammed Roshan <158417373+RoshanAndro@users.noreply.github.com>
2025-03-05 15:27:53 +0530

    Add files via upload

---
+++ Cpuinfo.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+class Cpu {
+    int price;
+
+    class Processor {
+        String no_of_cores, manufacturer;
+
+        void display() {
+            System.out.println("Processor Manufacturer : " + manufacturer);
+            System.out.println("No of cores of processor : " + no_of_cores);
+        }
+    }
+
+    static class Ram {
+        int memory;
+        String manufacturer;
+
+        void display() {
+            System.out.println("RAM Memory : " + memory);
+            System.out.println("RAM Manufacturer : " + manufacturer);
+        }
+    }
+
+    void display() {
+        System.out.println("Price : " + price);
+    }
+}
+
+public class Cpuinfo {
+    public static void main(String[] args) {
+        Cpu ncpu = new Cpu();
+        Cpu.Processor processor = ncpu.new Processor();
+        Cpu.Ram ram = new Cpu.Ram();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the processor Manufacturer : ");
+        processor.manufacturer = sc.next();
+
+        System.out.println("Enter the no of cores of the processor : ");
+        processor.no_of_cores = sc.next();
+
+        System.out.println("Enter the RAM memory Size (in GB): ");
+        ram.memory = sc.nextInt();
+
+        System.out.println("Enter the RAM Manufacturer : ");
+        ram.manufacturer = sc.next();
+
+        System.out.println("Enter the price of the CPU : ");
+        ncpu.price = sc.nextInt();
+
+        System.out.println("\nInformation of processor and RAM: ");
+        processor.display();
+        ram.display();
+        ncpu.display();
+    }
+}
