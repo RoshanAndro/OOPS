a4c2146710dd7956903b20d118f6f0eed169e433
Mohammed Roshan <158417373+RoshanAndro@users.noreply.github.com>
2025-03-21 15:22:31 +0530

    Add files via upload

---
+++ Linear.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+class Linear {
+    void define(int arr[], int n, int item) {
+        int i;
+        boolean found = false;
+        
+        for(i = 0; i < n; i++) {
+            if(arr[i] == item) {
+                System.out.println("Item found at position: " + (i + 1));
+                found = true;
+            }
+        }
+        
+        if (!found) {
+            System.out.println("Item not found in the array.");
+        }
+    }
+
+    public static void main(String args[]) {
+        int n, item, i;
+        Scanner sc = new Scanner(System.in);
+        Linear lin = new Linear();
+        
+        System.out.print("Enter the number of elements: ");
+        n = sc.nextInt();
+        
+        int[] arr = new int[n];
+        System.out.println("Enter " + n + " numbers: ");
+        for(i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        System.out.println("Enter the number to be searched: ");
+        item = sc.nextInt();
+        
+        lin.define(arr, n, item);
+    }
+}
