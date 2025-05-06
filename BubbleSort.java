dd5a5bd4d0c36bea2230ccdfc561d0bbd4eafc0b
Mohammed Roshan <158417373+RoshanAndro@users.noreply.github.com>
2025-05-06 14:59:47 +0530

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+class BubbleSort {
+    
+    public static <T extends Comparable<T>> void bubbleSort(T[] array) {
+        int n = array.length;
+        boolean swapped;
+
+        
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (array[j].compareTo(array[j + 1]) > 0) {
+                    T temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+        Integer[] array = new Integer[n];
+
+        System.out.println("Enter " + n + " integers:");
+        for (int i = 0; i < n; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        bubbleSort(array);
+        
+        System.out.println("Sorted array: " + Arrays.toString(array));
+        scanner.close();
+    }
+}
